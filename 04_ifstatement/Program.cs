9f3ba6b04d8cec9a5be1332aacc6bee2ea5204a9
Anastasia Klochko <Anastasiya.Klochko@Kinross.com>
2022-06-05 23:55:56 +0700

    4. if statement

---
+++ 04_ifstatement/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Hi, enter username ");
+string username = Console.ReadLine();
+
+if(username.ToUpper() == "V")
+{
+    Console.WriteLine("8>");
+}
+else
+{
+    Console.Write("Nice to meet u");
+    Console.WriteLine(username);
+}
