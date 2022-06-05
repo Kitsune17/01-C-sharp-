bfdd9a4e0e121c40fd84bb444da9688c8b2944e2
Anastasia Klochko <Anastasiya.Klochko@Kinross.com>
2022-06-05 23:31:08 +0700

    relocated folder 03_Sum

---
+++ 03_Sum/Program.cs
@@ -0,0 +1,5 @@
+﻿double number1 = new Random().Next(1, 100);
+double number2 = new Random().Next(1, 100);
+Console.WriteLine(number1);
+Console.WriteLine(number2);
+Console.WriteLine(number1 + number2);
