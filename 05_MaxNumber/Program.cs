21587c18a64cbf11ccc0353ff950ae46181c9232
Anastasia Klochko <Anastasiya.Klochko@Kinross.com>
2022-06-06 00:45:44 +0700

    5. Max number

---
+++ 05_MaxNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] a = {1, 11, 8, 4, 5};
+int index = 0;
+int max = 0;
+int size = 5;
+while(index < size)
+{
+    if(a[index] > max)
+    {
+        max = a[index];
+    }
+    index ++;
+}
+
+Console.Write("Max number is ");
+Console.WriteLine(max);
