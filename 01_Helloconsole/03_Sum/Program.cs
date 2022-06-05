29f69e580b83b6401fad5ebe1a073d7454bc43aa
Anastasia Klochko <Anastasiya.Klochko@Kinross.com>
2022-06-05 23:31:58 +0700

    Commit new strusture

--- 01_Helloconsole/03_Sum/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿double number1 = new Random().Next(1, 100);
-double number2 = new Random().Next(1, 100);
-Console.WriteLine(number1);
-Console.WriteLine(number2);
-Console.WriteLine(number1 + number2);
